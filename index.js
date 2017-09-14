18ea3ff5617fb80cd3e41336e6867e13f83081e9
Li, Huanlin <huanlin.li@teamaol.com>
2017-09-14 14:45:32 -0400

    chatbot

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ app.post('/webhook', function(req, res) {
 	let messaging_events = req.body.messaging;
 	var text = ""
 	for(let i = 0; i < messaging_events.length; i++) {
-		let event = JSON.parse(messaging_events[i]);
+		let entry = messaging_events[i];
+		event = JSON.parse(entry);
 		console.log("request body" + JSON.stringify(event.text));
 		let sender = event.id;
 		if(event.text) {
